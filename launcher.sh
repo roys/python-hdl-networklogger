81d6185d0321cec2f28b1181627538d5466e7775
Roy Solberg <git@roysolberg.com>
2016-04-23 19:18:36 +0200

    Launcher more or less ready. Updated docs.

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo Waiting a bit before starting script...
 cd ~/python-hdl-networklogger
 /bin/sleep 30	# Just to be more sure that the network interfaces are up and running
+echo Starting script...
 /usr/bin/python /home/pi/python-hdl-networklogger/networklogger.py >/home/pi/python-hdl-networklogger/launcher.log 2>&1
